ca1cf35efdda7a1fcc60d8929df850bc9c51f191
mew <mew067353@gmail.com>
2025-08-12 21:04:41 +0200

    Add files via upload

--- translations/translations.js
+++ translations/translations.js
@@ -3,24 +3,28 @@
 const userLang = navigator.language || navigator.userLanguage;
 const translations = {
   fr: {
+    templateNotice: `Ce script permet d'afficher les templates : <a href="https://bluemarble.camilledaguin.fr/" target="_blank" rel="noopener noreferrer">bluemarble.camilledaguin.fr</a>.`,
     grief: "En cas de vandalisme :",
     thanks: "Merci d'avoir pris le temps de scanner le QR code ! Ce pixel art est un projet communautaire et j'apprécierais énormément toute aide pour le maintenir ou l'améliorer ❤️.",
     hudPause: "espace pour mettre pause",
     tooltipZoomOut: "clique droit pour dézoomer"
   },
   de: {
+    templateNotice: `Dieses Skript zeigt die Vorlagen an: <a href="https://bluemarble.camilledaguin.fr/" target="_blank" rel="noopener noreferrer">bluemarble.camilledaguin.fr</a>.`,
     grief: "Bei Vandalismus :",
     thanks: "Danke, dass Sie sich die Zeit genommen haben, den QR-Code zu scannen! Dieses Pixel-Art ist ein Gemeinschaftsprojekt und jede Hilfe, es zu erhalten oder zu verbessern, wird sehr geschätzt ❤️.",
     hudPause: "Leertaste zum Pausieren",
     tooltipZoomOut: "rechtsklick zum herauszoomen"
   },
   it: {
+    templateNotice: `Questo script consente di visualizzare i template: <a href="https://bluemarble.camilledaguin.fr/" target="_blank" rel="noopener noreferrer">bluemarble.camilledaguin.fr</a>.`,
     grief: "In caso di vandalismo :",
     thanks: "Grazie per aver preso il tempo di scansionare il QR code! Questo pixel art è un progetto comunitario e apprezzerei moltissimo qualsiasi aiuto per mantenerlo o migliorarlo ❤️.",
     hudPause: "spazio per mettere in pausa",
     tooltipZoomOut: "clic destro per zoom indietro"
   },
   en: {
+    templateNotice: `This script allows displaying the templates: <a href="https://bluemarble.camilledaguin.fr/" target="_blank" rel="noopener noreferrer">bluemarble.camilledaguin.fr</a>.`,
     grief: "In case of grief :",
     thanks: "Thank you for taking the time to scan the QR code! This pixel art is a community project, and I would greatly appreciate any help to maintain or improve it ❤️.",
     hudPause: "space to pause",
@@ -31,9 +35,9 @@ const translations = {
 const langCode = userLang.substring(0, 2).toLowerCase();
 const messages = translations[langCode] || translations.en;
 
+document.getElementById("template-notice").innerHTML = messages.templateNotice;
 document.getElementById("grief-link").innerHTML =
   `${messages.grief} <a href="https://www.reddit.com/r/WplaceLive/comments/1mew9yn/report_users/" target="_blank" rel="noopener noreferrer">https://www.reddit.com/r/WplaceLive/comments/1mew9yn/report_users/</a>`;
-
 document.getElementById("thanks-message").innerHTML = messages.thanks;
 
 const hudText = document.getElementById("hud-text");
